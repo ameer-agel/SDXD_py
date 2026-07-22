94969a220a09f5a1ba2a85b64aa2968229966cef
Ameer Aqel <ameer_agel@hotmail.com>
2026-07-22 16:27:30 +0300

    chore: update database connection settings to Aiven instance defaults

--- db_configuration.py
+++ db_configuration.py
@@ -3,10 +3,10 @@ import os
 
 def get_db_connection():
     return pymysql.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
+        host=os.getenv("DB_HOST", "sdxd-600d9b6-sdxd-0a92.h.aivencloud.com"),
+        user=os.getenv("DB_USER", "avnadmin"),
+        password=os.getenv("DB_PASSWORD", "AVNS_sdMBiUIUBAoE2JmHVqO"),
         database=os.getenv("DB_NAME", "SDXD"),
-        port=int(os.getenv("DB_PORT", 3306)),
+        port=int(os.getenv("DB_PORT", 26352)),
         cursorclass=pymysql.cursors.DictCursor,
     )
